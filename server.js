d1dafdc16d49e4bbb7f69b15f88e2a7254887c07
Leandro <73486095+leandro144@users.noreply.github.com>
2022-03-03 19:09:54 -0300

    test

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const { resolve } = require('path')
-const app = express()
+const app = express();
 
 app.use('/',
- express.static(
-     resolve(
-        __dirname,
-        './build'
-     )
-    )
- )
+ express.static()
+)
 
 app.listen(process.env.PORT || 8080, (err) => {
     if (err) { return console.log(err)}
