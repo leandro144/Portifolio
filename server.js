c54ca92e099eb663548858746045a8d5e62448d9
Leandro <73486095+leandro144@users.noreply.github.com>
2022-03-03 18:55:04 -0300

    deploy commit

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('batata')
+const express = require('express');
+const { resolve } = require('path')
+const app = express()
+
+app.use('/',
+ express.static(
+     resolve(
+        __dirname,
+        './build'
+     )
+    )
+ )
+
+app.listen(process.env.PORT || 8080, (err) => {
+    if (err) { return console.log(err)}
+
+    console.log('tudo funcionando certinho')
+})
